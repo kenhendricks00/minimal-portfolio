60862d70539ecffff9ea94e1f4e39d9f880d896b
Kenneth Hendricks <50819541+kenhendricks00@users.noreply.github.com>
2023-03-17 16:20:44 -0400

    Update portfolio.js

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -4,5 +4,11 @@ export default [
         imgUrl: '/assets/dev-portfolio.jpg',
         stack: ['React', 'Tailwind', 'Vite'],
         link: 'https://kenhendricks.me',
+    },
+    {
+        title: 'Partycord - Discord Bot',
+        imgUrl: '/assets/partycord.jpg',
+        stack: ['Node.js', 'Discord.js', 'HTML', 'Bootstrap'],
+        link: 'https://partycord.xyz',
     }
 ]
