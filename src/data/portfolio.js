ded7409bf67b39be068140c96f8e2fc981f1854e
Kenneth Hendricks <50819541+kenhendricks00@users.noreply.github.com>
2023-11-07 20:36:07 -0500

    Update portfolio.js
    Added Emojis

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,18 +1,18 @@
 export default [
     {
-        title: 'Developer Portfolio',
+        title: '💻 Developer Portfolio',
         imgUrl: '/assets/dev-portfolio.jpg',
         stack: ['React', 'Tailwind', 'Vite'],
         link: 'https://kenhendricks.me',
     },
     {
-        title: 'Partycord - Discord Bot',
+        title: '🥳 Partycord - Discord Bot',
         imgUrl: '/assets/partycord.jpg',
         stack: ['Node.js', 'Discord.js', 'HTML', 'Bootstrap'],
         link: 'https://partycord.xyz',
     },
     {
-        title: 'More Projects',
+        title: '🧰 More Projects',
         imgUrl: '/assets/projects.jpg',
         stack: ['Fun', 'Useful', 'Interesting'],
         link: 'https://kenhendricks.me/projects',
